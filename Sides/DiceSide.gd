cc5d0ce13ba912a2fcbf7964b9ae7b45fbce7dd0
SomeSortOfSam <57550537+Theportalmasters@users.noreply.github.com>
2022-07-16 15:32:07 -0500

    Pip placement dictionary created

--- Sides/DiceSide.gd
+++ Sides/DiceSide.gd
@@ -3,6 +3,8 @@ class_name DiceSide
 
 var death_timer : Timer
 
+const PIP_DICTONARY := [[Vector2.ZERO],[Vector2.ONE,-Vector2.ONE],[Vector2.ONE,-Vector2.ONE,Vector2.ZERO],[Vector2.ONE,-Vector2.ONE,Vector2(-1,1),Vector2(1,-1)],[Vector2.ONE,-Vector2.ONE,Vector2(-1,1),Vector2(1,-1),Vector2.ZERO],[Vector2.ONE,-Vector2.ONE,Vector2(-1,1),Vector2(1,-1),Vector2.LEFT,Vector2.RIGHT]]
+
 var turns_lived := 0
 var scale_target := Vector3.ONE
 
@@ -15,10 +17,17 @@ func _ready():
 func _process(delta):
 	scale = scale.lerp(scale_target,delta/death_timer.wait_time)
 
-func prepare_side(number,previous_direction):
+func prepare_side(number : int ,previous_direction : Vector2):
 	previous_direction.y *= -1
 	rotate(basis.y,previous_direction.angle() + PI/2)
 	scale *= .1
+
+func place_one_on_each_pip(template : Node3D, number : int):
+	for i in number:
+		var pip = template.duplicate()
+		var pip_place = PIP_DICTONARY[number-1][i]
+		pip.position = (Vector3(pip_place.x,0,pip_place.y))*.6
+		add_child(pip)
 
 func on_cube_rotation_finished():
 	if turns_lived > 1:
--- Sides/Grassland/Grassland.gd
+++ Sides/Grassland/Grassland.gd
@@ -1,4 +1,10 @@
 extends DiceSide
 
+@onready var grass = $Grass
 
+func prepare_side(number,previous_direction):
+	super(number,previous_direction)
+	remove_child(grass)
+	place_one_on_each_pip(grass,number)
+	grass.queue_free()
 
