346bb3c592c36229f42bebf5362c81e6f3a5fa3f
SomeSortOfSam <57550537+Theportalmasters@users.noreply.github.com>
2022-07-16 12:11:26 -0500

    Assigned numbers to sides

--- Cube.gd
+++ Cube.gd
@@ -1,11 +1,11 @@
 extends Node3D
 
-@onready var side_positions = $SidePositions
 @onready var rotation_timer : Timer = $RotationTimer
 
 @export var time_to_rotate := .2
 @export var sides : Array[PackedScene]
 
+const NORMALS_TO_NUMBERS := {Vector3i.UP:1,Vector3i.DOWN:6,Vector3i.LEFT:2,Vector3i.RIGHT:5,Vector3i.FORWARD:4,Vector3i.BACK:3}
 
 var target_rotation : Quaternion
 var current_side_template : Node3D
@@ -13,39 +13,40 @@ var current_side_index := 0
 
 signal rotation_finished
 
+func _ready():
+	prepare_current_side(Vector2.DOWN).turns_lived = 1
+
 func _process(delta):
 	quaternion = quaternion.slerp(target_rotation,delta/time_to_rotate).normalized()
 
-func get_current_side_parent() -> Node3D:
-	return side_positions.get_child(0)
-
 func get_current_side_number() -> int:
-	return 1;
+	return NORMALS_TO_NUMBERS[Vector3i(basis.z.round())];
 
 func get_next_side() -> Node3D:
 	if not current_side_template:
 		current_side_template = sides[current_side_index].instantiate()
 	return current_side_template.duplicate()
 
-func prepare_current_side():
-	var side_parent = get_current_side_parent()
-	for child in side_parent.get_children():
-		side_parent.remove_child(child)
+func prepare_current_side(direction: Vector2) -> Node3D:
 	var side = get_next_side()
-	if side.has_method("prepared_side"):
-		side.prepare_side(get_current_side_number())
-		side_parent.add_child(side)
+	if side.has_method("prepare_side"):
+		side.call_deferred("prepare_side",get_current_side_number(),direction)
+		connect("rotation_finished",side.on_cube_rotation_finished)
+		add_child(side)
+		side.global_rotation = Vector3.ZERO
+		side.global_position = Vector3.ZERO
+	return side
 	
 
-func _on_player_edge_reached(direction):
+func _on_player_edge_reached(direction : Vector2):
 	var old_rotaiton = quaternion
 	direction = direction * (PI/2)
 	global_rotate(Vector3.FORWARD,direction.x)
 	global_rotate(Vector3.RIGHT,direction.y)
 	target_rotation = quaternion
+	prepare_current_side(direction/(PI/2))
 	quaternion = old_rotaiton
 	rotation_timer.start(time_to_rotate)
-	prepare_current_side()
 
 func _on_rotation_timer_timeout():
 	emit_signal("rotation_finished")
---
+++ Sides/TestSide.gd
@@ -0,0 +1,29 @@
+extends Node3D
+
+@onready var sprite = $Sprite3D
+@onready var death_timer : Timer = $DeathTimer
+
+var turns_lived := 0
+var scale_target := Vector3.ONE
+
+func _process(delta):
+	scale = scale.lerp(scale_target,delta/death_timer.wait_time)
+
+func prepare_side(number,previous_direction):
+	remove_child(sprite)
+	for i in number:
+		var new_sprite = sprite.duplicate()
+		new_sprite.position = Vector3(previous_direction.x ,0,previous_direction.y) * i/number *.8
+		add_child(new_sprite)
+	sprite.queue_free()
+	scale *= .1
+
+func on_cube_rotation_finished():
+	if turns_lived > 1:
+		scale_target = Vector3.ZERO
+		death_timer.start() 
+	else:
+		turns_lived += 1
+
+func _on_death_timer_timeout():
+	queue_free()
