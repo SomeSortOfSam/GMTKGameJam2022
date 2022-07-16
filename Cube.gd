e26dfd6ea79e9b804fe7117752104d47d7675147
SomeSortOfSam <57550537+Theportalmasters@users.noreply.github.com>
2022-07-16 08:01:05 -0500

    Prevented player from going off the same edge they came from

--- Cube.gd
+++ Cube.gd
@@ -1,9 +1,41 @@
 extends Node3D
 
+@onready var side_positions = $SidePositions
+@onready var rotation_timer : Timer = $RotationTimer
+
+@export var time_to_rotate := .2
+@export var sides : Array[PackedScene]
+
+
 var target_rotation : Quaternion
+var current_side_template : Node3D
+var current_side_index := 0
+
+signal rotation_finished
 
 func _process(delta):
-	quaternion = quaternion.slerp(target_rotation,.1).normalized()
+	quaternion = quaternion.slerp(target_rotation,delta/time_to_rotate).normalized()
+
+func get_current_side_parent() -> Node3D:
+	return side_positions.get_child(0)
+
+func get_current_side_number() -> int:
+	return 1;
+
+func get_next_side() -> Node3D:
+	if not current_side_template:
+		current_side_template = sides[current_side_index].instantiate()
+	return current_side_template.duplicate()
+
+func prepare_current_side():
+	var side_parent = get_current_side_parent()
+	for child in side_parent.get_children():
+		side_parent.remove_child(child)
+	var side = get_next_side()
+	if side.has_method("prepared_side"):
+		side.prepare_side(get_current_side_number())
+		side_parent.add_child(side)
+	
 
 func _on_player_edge_reached(direction):
 	var old_rotaiton = quaternion
@@ -12,4 +44,8 @@ func _on_player_edge_reached(direction):
 	global_rotate(Vector3.RIGHT,direction.y)
 	target_rotation = quaternion
 	quaternion = old_rotaiton
-	
+	rotation_timer.start(time_to_rotate)
+	prepare_current_side()
+
+func _on_rotation_timer_timeout():
+	emit_signal("rotation_finished")
--- Player.gd
+++ Player.gd
@@ -1,12 +1,16 @@
 extends Node3D
 
-@onready var move_timer : Timer = $MoveTimer
+@onready var move_diffrence_timer : Timer = $MoveDiffrenceTimer
+@onready var move_durration_timer : Timer = $MoveDurrationTimer
 
 const MOVE_DISTANCE = .2
 const GRID_EXTENTS := 4
 const DIRECTION_MAP : Dictionary = {"ui_left" : Vector2i.LEFT,"ui_right" : Vector2i.RIGHT,"ui_up" : Vector2i.UP,"ui_down": Vector2i.DOWN}
 
-var last_move : Vector2i
+@export var position_lerp_enabled := true
+@export var time_to_rotate := .2
+
+var last_direction := Vector2i(0,-1)
 var queued_move : Vector2i
 var current_cell := Vector2i(0,GRID_EXTENTS)
 
@@ -16,28 +20,47 @@ func _unhandled_input(event):
 	for direction in DIRECTION_MAP:
 		if event.is_action_pressed(direction):
 			queued_move += DIRECTION_MAP[direction]
-			if move_timer.is_stopped():
-				move_timer.start()
+			if move_diffrence_timer.is_stopped():
+				move_diffrence_timer.start()
 
 func _process(delta):
-	position = position.lerp(Vector3(current_cell.x,0,current_cell.y)*MOVE_DISTANCE,.1)
-	quaternion = quaternion.slerp(Quaternion.IDENTITY,.1)
+	var percent_amount = delta/time_to_rotate
+	if position_lerp_enabled:
+		var target = Vector3(current_cell.x,0,current_cell.y) * MOVE_DISTANCE
+		global_position = global_position.lerp(target,percent_amount)
+	quaternion = quaternion.slerp(Quaternion.IDENTITY,percent_amount)
 
-func set_current_cell(new_cell : Vector2i):
-	current_cell = new_cell
-	if abs(current_cell.x) > GRID_EXTENTS:
-		current_cell.x = -sign(current_cell.x)*GRID_EXTENTS
-		emit_signal("edge_reached", sign(current_cell.x)*Vector2i.RIGHT)
-	elif abs(current_cell.y) > GRID_EXTENTS:
-		current_cell.y = -sign(current_cell.y)*GRID_EXTENTS
-		emit_signal("edge_reached", sign(current_cell.y)*Vector2i.UP)
-
-func _on_move_timer_timeout():
-	var queued_rotation = -Vector2(queued_move)*(PI/2)
-	rotate(Vector3.FORWARD,queued_rotation.x)
-	rotate(Vector3.RIGHT,queued_rotation.y)
-	set_current_cell(current_cell + queued_move)
-	queued_move = Vector2i.ZERO
+# returns true if the current cell changed
+func set_current_cell(new_cell : Vector2i) -> bool:
+	var new_direction = Vector2i.ZERO
+	if abs(new_cell.x) > GRID_EXTENTS:
+		new_direction = sign(new_cell.x)*Vector2i.RIGHT
+	elif abs(new_cell.y) > GRID_EXTENTS:
+		new_direction = sign(new_cell.y)*Vector2i.DOWN
+	if new_direction != -last_direction:
+		current_cell = new_cell
+		if new_direction != Vector2i.ZERO:
+			last_direction = new_direction
+			move_durration_timer.start(time_to_rotate)
+		return true
+	return false
 
 func _on_player_edge_reached(direction : Vector2i):
-	position = Vector3(direction.x,0,-direction.y)*MOVE_DISTANCE*(GRID_EXTENTS+1)
+	current_cell.x = sign(direction.x)*GRID_EXTENTS if direction.x != 0 else current_cell.x
+	current_cell.y = sign(direction.y)*GRID_EXTENTS if direction.y != 0 else current_cell.y
+	position_lerp_enabled = false
+
+func _on_cube_rotation_finished():
+	position_lerp_enabled = true
+
+func _on_move_diffrence_timer_timeout():
+	if set_current_cell(current_cell + queued_move):
+		var queued_rotation = -Vector2(queued_move)*(PI/2)
+		global_rotate(Vector3.FORWARD,queued_rotation.x)
+		global_rotate(Vector3.RIGHT,queued_rotation.y)
+	else:
+		global_rotate(Vector3.DOWN,.2)
+	queued_move = Vector2i.ZERO
+
+func _on_move_durration_timer_timeout():
+	emit_signal("edge_reached",-last_direction)
